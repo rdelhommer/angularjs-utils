e21bf53f22f1d7c991b57e1ded97c0f8e3636a2b
Rick Delhommer <rdelhommer@gmail.com>
2017-07-28 10:19:32 -0700

    remove core src and use package instead

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 (function () {
   'use strict';
 
-  angular.module('rdelhommer.ng.base',[]);
   angular.module('rdelhommer.ng.directives',[]);
-  angular.module('rdelhommer.ng.services',['rdelhommer.ng.base']);
+  angular.module('rdelhommer.ng.services',['rdelhommer.ng.core']);
 }());
